9eeba722972c927a5ecbf4ac449b6fce65cf9d93
Brian Green <b.agreen92@gmail.com>
2020-09-01 19:35:15 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './pages/Homepage/Homepage';
 import Header from './/components/Header/Header.component'
 import Navigation from './components/Navigation/Navigation.component';
 import Footer from './components/Footer/Footer.component';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import OurServices from './pages/Our-Services/Our-Services';
 import Contact from './pages/Contact/Contact';
 import TaxChecklist from './pages/Tax-Checklist/Tax-Checklist';
@@ -15,11 +15,14 @@ function App() {
     <div className="App">
       <Header />
       <Navigation />
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/our-services' component={OurServices} />
-      <Route exact path='/tax-checklist' component={TaxChecklist} />
-      <Route exact path='/related-links' component={RelatedLinks} />
-      <Route exact path='/contact' component={Contact} />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/our-services' component={OurServices} />
+        <Route exact path='/tax-checklist' component={TaxChecklist} />
+        <Route exact path='/related-links' component={RelatedLinks} />
+        <Route exact path='/contact' component={Contact} />
+      </Switch>
+      
       <Footer />
     </div>
   );
